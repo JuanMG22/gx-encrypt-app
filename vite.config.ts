642ffa04bbb039e65529d3447e98a49d3e707853
JUAN MANUEL GONZALEZ <juan.m-gonzalez@outlook.com>
2025-07-04 12:26:43 -0300

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite';
 import path from "path";
+import netlify from "@netlify/vite-plugin";
+
 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), netlify()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
